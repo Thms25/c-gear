05c21d74e70659e0f8960e959c82085026460645
Gabrielle del Marmol <129516150+Gdelmarl0l@users.noreply.github.com>
2023-05-29 15:19:28 +0200

    added show method

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -1,5 +1,17 @@
 class GearsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @gears = Gear.all
   end
+
+  def show
+  end
+
+  private
+
+  def gear_params
+    @gear = Gear.find(params[:id])
+  end
+
 end
