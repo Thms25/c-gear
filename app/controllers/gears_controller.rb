7e7d4e01f882136eff32fa110589d3adb8d97f30
thomas allen <thomasallenmartinho@gmail.com>
2023-06-01 11:04:39 +0200

    popups kind fixed

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -5,11 +5,15 @@ class GearsController < ApplicationController
     @gears = Gear.all
     @users = User.all
     @markers = @users.geocoded.map do |user|
-      {
+      gear = user.gears.first unless user.gears.empty?
+      marker = {
         lat: user.latitude,
         lng: user.longitude,
-        preview_html: render_to_string(partial: "preview", locals: {user: user})
       }
+      if gear.present?
+        marker[:preview_html] = render_to_string(partial: "preview", locals: {gear: gear})
+      end
+      marker
     end
   end
 
